fc0d8521483d9cf2420ff8d14155931e09dcc1a8
Arseniy Kucher <beeffallo@gmail.com>
2020-04-28 15:45:59 +0300

    Revert "freeze benchmark-settings"
    `equatable` suits better here
    This reverts commit ecfaa9c1414ff80a52cd73f574af6ef57400167b.

--- benchmark/lib/settings.dart
+++ benchmark/lib/settings.dart
@@ -11,69 +11,59 @@ enum Mode { wake, read, write, delete }
 enum Storage { single, multi, ether }
 enum Aspect { lock, mode, count, storage, size }
 
-@freezed
-abstract class BenchmarkSettings implements _$BenchmarkSettings {
-  const BenchmarkSettings._();
-  const factory BenchmarkSettings({
-    @Default(true)
-        bool uiLock,
-    @Default(false)
-        bool useAES,
-    @Default(false)
-        bool useB64,
-    @Default(<Mode, bool>{
-      Mode.wake: true,
-      Mode.read: true,
-      Mode.write: true,
-      Mode.delete: false,
-    }) // what to benchmark
-        Map<Mode, bool> modes,
-    @Default(<Storage, bool>{
-      Storage.single: true,
-      Storage.multi: true,
-      Storage.ether: true,
-    }) //where to benchmark
-        Map<Storage, bool> storages,
-    @Default(RangeValues(adjust(5), adjust(35)))
-        RangeValues blocCount,
-  }) = _BenchmarkSettings;
+class BenchmarkSettings {
+  var uiLock = true;
+  var useAES = false;
+  var useB64 = false;
+  void flipUseAES() => useAES = !useAES;
+  void flipUseB64() => useB64 = !useB64;
 
-  BenchmarkSettings flipUseAES() => copyWith(useAES: !useAES);
-  BenchmarkSettings flipUseB64() => copyWith(useB64: !useB64);
+  var modes = <Mode, bool>{
+    Mode.wake: true,
+    Mode.read: true,
+    Mode.write: true,
+    Mode.delete: false,
+  }; // what to benchmark
+  void addMode(Mode mode) => modes[mode] = true;
+  void delMode(Mode mode) => modes[mode] = false;
+  void flipMode(Mode mode) => modes[mode] = !modes[mode];
 
-  BenchmarkSettings flipMode(Mode mode) =>
-      copyWith(modes: {...modes, mode: !modes[mode]});
-
-  BenchmarkSettings flipStorage(Storage storage) =>
-      copyWith(storages: {...storages, storage: !storages[storage]});
+  var storages = <Storage, bool>{
+    Storage.single: true,
+    Storage.multi: true,
+    Storage.ether: true,
+  }; //where to benchmark
+  void addStorage(Storage storage) => storages[storage] = true;
+  void delStorage(Storage storage) => storages[storage] = false;
+  void flipStorage(Storage storage) => storages[storage] = !storages[storage];
 
   final blocCountRange = RangeValues(1, 50);
   final blocCountDivs = 10;
   // get coef => (blocCountRange.end - blocCountRange.start) / blocCountDivs;
   static double adjust(double n) => 1 + n * 49 / 50;
   // var blocCount = RangeValues(5.9, 35.3); // just amt (0-50)
-  // final blocCount = RangeValues(adjust(5), adjust(35)); // just amt (0-50)
+  var blocCount = RangeValues(adjust(5), adjust(35)); // just amt (0-50)
   RangeLabels get blocCountLabels => RangeLabels(
         '${blocCount.start.toInt()}',
         '${blocCount.end.toInt()}',
       );
 
-  // // int64 size is 4 bytes
-  // // string char 2 or 4 bytes
-  // final stateSizeRange = RangeValues(2, 22);
-  // final stateSizeDivs = 10;
-  // final stateSize = RangeValues(2, 8); // 2^n size (0-20)
-  // RangeLabels get stateSizeLabels => RangeLabels(
-  //       _format(stateSize.start.toInt()),
-  //       _format(stateSize.end.toInt()),
-  //     );
-  // int get stateSizeBytesMax => _convert(stateSize.end);
-  // int _convert(double e) => pow(2, e).toInt();
-  // String _format(int size) {
-  //   final e = size % 10;
-  //   final p = const ['Bytes', 'KB', 'MB', 'GB'][size ~/ 10];
-  //   return '${pow(2, e)} $p';
-  // }
+  // int64 size is 4 bytes
+  // string char 2 or 4 bytes
+  final stateSizeRange = RangeValues(2, 22);
+  final stateSizeDivs = 10;
+  var stateSize = RangeValues(2, 8); // 2^n size (0-20)
+  RangeLabels get stateSizeLabels => RangeLabels(
+        _format(stateSize.start.toInt()),
+        _format(stateSize.end.toInt()),
+      );
+  int get stateSizeBytesMax => _convert(stateSize.end);
+  int _convert(double e) => pow(2, e).toInt();
+  String _format(int size) {
+    final e = size % 10;
+    final p = const ['Bytes', 'KB', 'MB', 'GB'][size ~/ 10];
+    return '${pow(2, e)} $p';
+  }
 }
 
 @freezed
@@ -93,7 +83,7 @@ class SettingsBloc extends HydratedBloc<SettingsEvent, BenchmarkSettings> {
 
   @override
   Stream<BenchmarkSettings> mapEventToState(SettingsEvent event) async* {
-    yield event.when(
+    event.when(
       flipUseAES: () => state.flipUseAES(),
       flipUseB64: () => state.flipUseB64(),
       flipMode: (mode) => state.flipMode(mode),
@@ -101,6 +91,7 @@ class SettingsBloc extends HydratedBloc<SettingsEvent, BenchmarkSettings> {
       newBlocCount: (count) => state.blocCount = count,
       newStateSize: (size) => state.stateSize = size,
     );
+    yield state;
   }
 
   @override
