7acac9722f9c7e1b73c4cbb5990ba8fbb3655d01
Arseniy Kucher <beeffallo@gmail.com>
2020-05-15 00:02:51 +0300

    wip-2: plots

--- benchmark/endpoint/main.py
+++ benchmark/endpoint/main.py
@@ -1,12 +1,6 @@
 from os import walk
 import json
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-
-# plt.style.use('bmh')
-# plt.style.use('fivethirtyeight')
-plt.style.use('ggplot')
 
 
 def main():
@@ -19,59 +13,56 @@ def main():
     plot(rr)
 
 
+def draw(rr, aes, mode, storage, count):
+    ff = [
+        aesFilter(aes),
+        # sizeFilter(2**2),
+        countFilter(count),
+        modeFilter(mode),
+        storageFilter(storage),
+    ]
+    data = applyFilters(ff, rr)
+    # data = sorted(data, key=lambda r: r['count'])
+    data = sorted(data, key=lambda r: r['size'])
+
+    xx = [x['count'] for x in data]
+    yy = [x['size'] for x in data]
+    zz = [x['intMeanMicroseconds'] for x in data]
+    ee = [x['intSDMicroseconds'] for x in data]
+
+    # {count}
+    # {'aes' if aes else 'non-aes'}
+    label = f"{mode} {storage}"
+    # ecolor='b', color='b'
+    plt.errorbar(yy, zz, yerr=ee, linestyle='-',
+                 marker='.', capsize=3, label=label)
+
+
+# fig, axs = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
 def plot(rr):
-    fig = plt.figure()
-    # for mode in ['read', 'write', 'wake']:
-    for mode in ['wake']:
-        for storage in ['single', 'multi', 'hive']:
-            # for count in [1, 15, 30, 75, 150]:
-            for count in [1, 150]:
-                ff = [
-                    aesFilter(True),
-                    # sizeFilter(2**2),
-                    countFilter(count),
-                    modeFilter(mode),
-                    storageFilter(storage),
-                ]
-                data = applyFilters(ff, rr)
-                # data = sorted(data, key=lambda r: r['count'])
-                data = sorted(data, key=lambda r: r['size'])
-
-                xx = [x['count'] for x in data]
-                yy = [x['size'] for x in data]
-                zz = [x['intMeanMicroseconds'] for x in data]
-                ee = [x['intSDMicroseconds'] for x in data]
-
-                # 2D
-                # plt.errorbar(yy, zz, yerr=ee, fmt='-o', linestyle='-', marker='.')
-
-                # label = ('aes' if storage else 'no-aes') + f', {count} blocs'
-                label = f'{count} aes {mode} {storage}'
-                plt.errorbar(yy, zz, yerr=ee, linestyle='-',
-                             marker='.', capsize=3, label=label)  # ecolor='b', color='b'
-                # plt.scatter(xx, yy)
-
-    # leg = ax.legend()
-    # for artist, text in zip(leg.legendHandles, leg.get_texts()):
-    #     col = artist.get_color()
-    #     if isinstance(col, np.ndarray):
-    #         col = col[0]
-    #     text.set_color(col)
-
-    # plt.xlabel('count')
-    plt.xlabel('Size, bytes')
-    plt.ylabel('Time, μs')
-    plt.yscale('log')
-    plt.xscale('log')
-    plt.legend(loc='lower right')
+    for aes in [True, False]:  # [True, False]
+        for mode in ['write']:  # ['read', 'write', 'wake']
+            i = 0
+            fig = plt.figure()
+            for count in [1, 30, 150]:
+                i += 1  # [1, 15, 30, 75, 150]
+                # plt.subplot(2, 1, 1)
+                # plt.plot(x1, y1, 'o-')
+                # plt.ylabel('Damped oscillation')
+                plt.style.use('ggplot')
+                # plt.title(f'One bloc. 1:AES. 2:NO AES')
+                plt.subplot(3, 1, i)
+                plt.title(f'{count} bloc{"s" if count > 1 else ""},' +
+                          f' {"aes" if aes else "no aes"}')
+                # plt.legend(loc='lower right')
+                for storage in ['single', 'multi', 'hive']:
+                    draw(rr, aes, mode, storage, count)
+                plt.xlabel('Size, bytes')
+                plt.ylabel('Time, μs')
+                plt.yscale('log')
+                plt.xscale('log')
+                plt.legend(loc='lower right')
     plt.show()
-
-    # 3D
-    # fig = plt.figure()
-    # ax = fig.gca(projection='3d')
-    # ax.scatter(xx, yy, zz, label='bench')
-    # ax.legend()
-    # plt.show()
 
 
 def aesFilter(flag):
@@ -100,15 +91,12 @@ def applyFilters(ff, rr):
     return list(rr)
 
 
-# (aes, storage, mode, count, size)
 def parse(files):
-    # return [r for file in files for r in getResults(file)]
     rr = []
     for file in files:
         count = file['settings']['blocCount']
         size = file['settings']['stateSize']
         for r in file['results']:
-            # key = (r['aes'], r['storage'], r['mode'], count, size)
             r['count'] = count
             r['size'] = size
             rr.append(r)
